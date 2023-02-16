89e8ac2707b7b250718e620d0fee851efe9b32e0
shashank singh <shashanksngh707@gmail.com>
2023-02-16 23:27:05 +0530

    added collection examples

---
+++ src/main/java/org/shashank/collections/HashTableExample.java
@@ -0,0 +1,28 @@
+package org.shashank.collections;
+
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+
+/*
+
+Hash Map vs Hash Table
+Hash map is non-synchronized whereas Hash Table is synchronized which means only \
+one thread can access an elements at a time.
+Hash map is not thread safe whereas Hash table is thread safe.
+Hash table performance is poorer compared to Hash map.
+Null keys and values are not allowed in Hash table.
+ */
+public class HashTableExample {
+
+    public static void main(String[] args) {
+
+        Hashtable<Integer, String> ht = new Hashtable<>();
+        ht.put(1, "Shashank");
+        ht.put(2, "Singh");
+
+        for(Map.Entry<Integer, String> e: ht.entrySet()){
+            System.out.println(e.getValue());
+        }
+    }
+}
---
+++ src/main/java/org/shashank/collections/LinkedListExample.java
@@ -0,0 +1,32 @@
+package org.shashank.collections;
+
+import java.util.LinkedList;
+
+/*
+Use Linked List when you have more insertion deletion as ArrayList requires shifting of elements
+which is time taking. Linked List does not store elements sequentially. Don't use Linked List if you have
+more retrieving operations
+Node contains - prev, data, next
+Duplicates allowed, insertion order is preserved
+Null is allowed
+Additional Methods of Linked List class-
+assFirst(Object o)
+addLast(Object o)
+removeFirst()
+removeLast()
+getFirst()
+getLast()
+iterator()
+ */
+public class LinkedListExample {
+
+    public static void main(String[] args){
+
+        LinkedList<String> l = new LinkedList<>();
+        l.add("A");
+        l.addFirst("B");
+        l.addLast("C");
+        l.forEach(System.out::println);
+    }
+}
+
