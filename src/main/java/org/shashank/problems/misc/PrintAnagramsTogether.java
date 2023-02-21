0367738aad304e883e98f8da88357ee23ea7b5b7
shashank singh <shashanksngh707@gmail.com>
2023-02-21 23:54:18 +0530

    arrays

---
+++ src/main/java/org/shashank/problems/misc/PrintAnagramsTogether.java
@@ -0,0 +1,27 @@
+package org.shashank.problems.misc;
+
+import java.util.*;
+
+public class PrintAnagramsTogether {
+
+    public static void printAnagrams(String[] words){
+        Map<String, List<String>> result = new HashMap<>();
+        char[] charSequence;
+        String sortedWord;
+        for(String word: words){
+            charSequence = word.toCharArray();
+            Arrays.sort(charSequence);
+            sortedWord = new String(charSequence);
+            if(!result.containsKey(sortedWord)){
+                result.put(sortedWord, new ArrayList<>());
+            }
+            result.get(sortedWord).add(word);
+        }
+        result.values().forEach(System.out::println);
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"cat", "dog", "tac", "god", "act"};
+        printAnagrams(words);
+    }
+}
