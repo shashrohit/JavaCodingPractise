482bbe3be3c3771f743bb58b06da4a6350177c97
shashank singh <shashanksngh707@gmail.com>
2023-02-22 17:38:55 +0530

    sorting

--- src/main/java/org/shashank/problems/strings/Rotation.java
+++ src/main/java/org/shashank/problems/strings/Rotation.java
@@ -2,4 +2,16 @@ package org.shashank.problems.strings;
 
 public class Rotation {
 
+    public static void main(String[] args) {
+        // Left rotate
+        String str1 = "GeeksforGeeks";
+        int d = 2;
+        //O(n)
+        System.out.println(str1.substring(d) + str1.substring(0, d));
+
+        int n = str1.length();
+        // Right rotate
+        System.out.println(str1.substring(n-d) + str1.substring(0, n-d));
+    }
+
 }
