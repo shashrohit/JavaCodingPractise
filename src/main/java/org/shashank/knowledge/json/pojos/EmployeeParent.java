ba5b1b35e4db41fb5e707b5ed956d519209b1979
shashank singh <shashanksngh707@gmail.com>
2023-02-23 13:45:34 +0530

    json

---
+++ src/main/java/org/shashank/knowledge/json/pojos/EmployeeParent.java
@@ -0,0 +1,24 @@
+package org.shashank.knowledge.json.pojos;
+
+public class EmployeeParent {
+
+    private EmpInfo empInfo;
+    private Skills[] skills;
+    public EmpInfo getEmpInfo() {
+        return empInfo;
+    }
+    public void setEmpInfo(EmpInfo empInfo) {
+        this.empInfo = empInfo;
+    }
+    public Skills[] getSkills() {
+        return skills;
+    }
+    public void setSkills(Skills[] skills) {
+        this.skills = skills;
+    }
+
+//Generate Getters and Setters for above 2 objects
+
+
+
+}
